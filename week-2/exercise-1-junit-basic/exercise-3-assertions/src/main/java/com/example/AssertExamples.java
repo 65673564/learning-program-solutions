935abbea4cdeb29b4e26c515fb92152eb4a7a5e5
65673564 <141355563+65673564@users.noreply.github.com>
2025-07-16 21:18:23 +0530

    Update AssertExamples.java

---
+++ week-2/exercise-1-junit-basic/exercise-3-assertions/src/main/java/com/example/AssertExamples.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class AssertExamples {
+
+    public String sayHello() {
+        return "Hello, Raji!";
+    }
+
+    public int[] getMarks() {
+        return new int[]{85, 90, 95};
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public String getNullValue() {
+        return null;
+    }
+}
