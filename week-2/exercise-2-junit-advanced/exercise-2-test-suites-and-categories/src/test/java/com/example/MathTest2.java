988fbf88dfeffd0d493388e2bd7afa5b7dc15e96
65673564 <141355563+65673564@users.noreply.github.com>
2025-07-16 21:33:20 +0530

    Update MathTest2.java

---
+++ week-2/exercise-2-junit-advanced/exercise-2-test-suites-and-categories/src/test/java/com/example/MathTest2.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathTest2 {
+    @Test
+    void testSubtract() {
+        assertEquals(1, 3 - 2);
+    }
+}
