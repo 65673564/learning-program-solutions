c2fa1636648175e5513e447d3b5a12063b5d7124
65673564 <141355563+65673564@users.noreply.github.com>
2025-07-16 21:32:54 +0530

    Update MathTest1.java

---
+++ week-2/exercise-2-junit-advanced/exercise-2-test-suites-and-categories/src/test/java/com/example/MathTest1.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathTest1 {
+    @Test
+    void testAdd() {
+        assertEquals(5, 2 + 3);
+    }
+}
