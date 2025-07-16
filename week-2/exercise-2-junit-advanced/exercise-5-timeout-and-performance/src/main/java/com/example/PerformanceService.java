dbcd4abca91c2bc156b8bec4ebca82ce0dcb95b9
65673564 <141355563+65673564@users.noreply.github.com>
2025-07-16 21:39:18 +0530

    Update PerformanceService.java

---
+++ week-2/exercise-2-junit-advanced/exercise-5-timeout-and-performance/src/main/java/com/example/PerformanceService.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class PerformanceService {
+
+    public void loadBigData() throws InterruptedException {
+        // Simulate delay (e.g., fetching from DB)
+        Thread.sleep(100); // 100 ms
+    }
+}
