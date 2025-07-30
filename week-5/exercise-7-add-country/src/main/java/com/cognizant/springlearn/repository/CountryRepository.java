f05210f7144a47fa9fc5f2d87903f6a2e4ef92d8
65673564 <141355563+65673564@users.noreply.github.com>
2025-07-30 21:42:49 +0530

    Update CountryRepository.java

---
+++ week-5/exercise-7-add-country/src/main/java/com/cognizant/springlearn/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.example.ormlearn.repository;
+
+import com.example.ormlearn.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, String> {
+    Country findByCode(String code);
+}
